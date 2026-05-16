5f5d4ba3bd75d204e146a6453c19e8e660c2628c
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2026-05-16 14:57:55 +0530

    Refresh audio player build

--- obj/x64/Debug/audioplayer.AssemblyInfo.cs
+++ obj/x64/Debug/audioplayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("audioplayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f989cd842b5ad88bed9c0752f6839bd2790ae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("audioplayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("audioplayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
